73df8bbbd666a5bac9d33b43358c1b61235612c0
hgedia <hgedia@hgedia.co>
2018-10-04 11:41:27 +0100

    Updating as per comments, test cases,cleanup of code

--- contracts/SubdomainRegistrar.sol
+++ contracts/SubdomainRegistrar.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.18;
 
 import "./Ownable.sol";
 
-interface ENS {
+interface IENS {
     function setSubnodeOwner(bytes32 node, bytes32 label, address owner) public;
     function setResolver(bytes32 node, address resolver) public;
     function setOwner(bytes32 node, address owner) public;
@@ -10,16 +10,6 @@ interface ENS {
     function owner(bytes32 node) public view returns (address);
     function resolver(bytes32 node) public view returns (address);
     function ttl(bytes32 node) public view returns (uint64);
-}
-
-
-interface IDefaultReverseResolver{
-     function setName(bytes32 node, string _name);
-}
-
-interface IReverseRegistrar{
-    function claim(address owner) public returns (bytes32);
-    function node(address addr) constant returns (bytes32);    
 }
 
 interface IPublicResolver{
@@ -34,67 +24,54 @@ interface IPublicResolver{
 
 
 contract SubdomainRegistrar is Ownable {
-    ENS public ens;
-    IPublicResolver public publicResolver;
-    IReverseRegistrar public reverseRegistrar;
-    IDefaultReverseResolver public defaultReverseResolver;
-
-    // namehash('addr.reverse')
-    bytes32 constant ADDR_REVERSE_NODE = 0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
     bytes32 public rootNode;
     string public rootName; 
     address public owner;
-
+    IENS public ens;
+    IPublicResolver public publicResolver;
 
     /**
      * Constructor.
      * @param _ens The address of the ENS registry.
      * @param _publicResolver The address of the public resolver
-     * @param _defaultReverseResolver The address of the default reverse resolver 
      * @param _rootNode The node that this registrar administers.i.e: namehash('transcoder.eth')
      * @param _rootName The name of the root node "trancoder.eth". 
      */
-    constructor(address _ens, address _publicResolver,address _defaultReverseResolver, bytes32 _rootNode, string _rootName) public {
-        ens = ENS(_ens);
+    constructor(address _ens, address _publicResolver, bytes32 _rootNode, string _rootName) public {
+        ens = IENS(_ens);
         publicResolver = IPublicResolver(_publicResolver);
-        reverseRegistrar = IReverseRegistrar(ens.owner(ADDR_REVERSE_NODE));
-        defaultReverseResolver = IDefaultReverseResolver(_defaultReverseResolver);
         rootNode = _rootNode;
         rootName = _rootName; 
     }
 
     /**
      * Register subdomain
-     * @param name The name in "<name>.trancoder.eth"
+     * @param _name The name in "<name>.trancoder.eth"
      */       
-    function registerSubdomain(string name) public {       
-        bytes32 subnode = keccak256(name);
-        require(ens.owner(keccak256(rootNode, subnode)) == address(0),"Subdomain is already registered");
+    function registerSubdomain(string _name) public {       
+        bytes32 subnode = keccak256(abi.encodePacked(_name));
+        require(ens.owner(keccak256(abi.encodePacked(rootNode, subnode))) == address(0),"Subdomain is already registered");
         ens.setSubnodeOwner(rootNode, subnode, address(this));
-        bytes32 fullHash = keccak256(rootNode, subnode);
+        bytes32 fullHash = keccak256(abi.encodePacked(rootNode, subnode));
         ens.setResolver(fullHash, address(publicResolver));
         publicResolver.setAddr(fullHash, msg.sender);
         ens.setSubnodeOwner(rootNode, subnode, msg.sender);
-        
-        //Reverse Registration
-        //NOTE: User must transfer reverse domain ownership to this contract before calling it.
-        string memory fullName = string(abi.encodePacked(name,".",rootName));
-        bytes32 reverseNode = reverseRegistrar.node(msg.sender);
-        ens.setResolver(reverseNode,defaultReverseResolver);
-        defaultReverseResolver.setName(reverseNode,fullName);       
     }
 
-    function transferDomainOwnership(address to) public onlyOwner{
-        ens.setOwner(rootNode,to);
+    /**
+     * Transfer domain ownership i.e "transcoder.eth"
+     * @param _to The address to which the domain ownership is transferred to.
+     */  
+    function transferDomainOwnership(address _to) public onlyOwner{
+        ens.setOwner(rootNode,_to);
     }
-
-    function transferNodeOwnership(bytes32 subnode,address newOwner) public onlyOwner {
-        ens.setSubnodeOwner(rootNode,subnode,newOwner);
-    }
-
-    function reverseRegistrarSetname(bytes32 node,string name) public onlyOwner {
-        address resolver = ens.resolver(node);
-        IDefaultReverseResolver revResolver = IDefaultReverseResolver(resolver);
-        revResolver.setName(node,name);
+    
+    /**
+     * Transfer node ownership i.e "hello.transcoder.eth"
+     * @param _subnode namehash of the subdomain to be transferred.
+     * @param _newOwner The address to which sub domain ownership is transferred to.
+     */
+    function transferNodeOwnership(bytes32 _subnode,address _newOwner) public onlyOwner {
+        ens.setSubnodeOwner(rootNode,_subnode,_newOwner);
     }
 }
--- test/TestSubdomainRegistrar.js
+++ test/TestSubdomainRegistrar.js
@@ -2,9 +2,6 @@ const ENS = artifacts.require('ENSRegistry.sol');
 const SubdomainRegistrar = artifacts.require('SubdomainRegistrar.sol');
 const PublicResolver = artifacts.require('PublicResolver.sol');
 const FIFSRegistrar = artifacts.require('FIFSRegistrar.sol');
-const DefaultReverseResolver = artifacts.require('DefaultReverseResolver.sol');
-const ReverseRegistrar = artifacts.require('ReverseRegistrar.sol');
-
 const utils = require('./helpers/Utils.js');
 const namehash = require('eth-ens-namehash');
 
@@ -17,43 +14,30 @@ contract('Subdomain Registrar', function (accounts) {
         node = namehash('eth');
         ens = await ENS.new();       
         resolver = await PublicResolver.new(ens.address);  
-        registrar = await FIFSRegistrar.new(ens.address, 0);               
-        defaultRevResolver = await DefaultReverseResolver.new(ens.address);
-        reverseRegistrar = await ReverseRegistrar.new(ens.address, defaultRevResolver.address);
-        await ens.setSubnodeOwner(0, web3.sha3('reverse'), accounts[0], { from: accounts[0] });
-        await ens.setSubnodeOwner(namehash('reverse'), web3.sha3('addr'), reverseRegistrar.address, { from: accounts[0] });       
+        registrar = await FIFSRegistrar.new(ens.address, 0);                
         await ens.setOwner(0, registrar.address, { from: accounts[0] });
-    });
 
-    it('Register subdomain', async () => {
         //Initialize data
-        await registrar.register(web3.sha3('eth'), accounts[0], {from: accounts[0]});
+        await registrar.register(web3.sha3('eth'), accounts[0], { from: accounts[0] });
 
         //Register domain transcoder.eth
-        await ens.setSubnodeOwner(namehash('eth'),web3.sha3('transcoder'),accounts[1], {from : accounts[0]})
-        assert.equal(await ens.owner(namehash('transcoder.eth')),accounts[1]);    
+        await ens.setSubnodeOwner(namehash('eth'), web3.sha3('transcoder'), accounts[1], { from: accounts[0] })
+        assert.equal(await ens.owner(namehash('transcoder.eth')), accounts[1]);
 
         //Setup Register subdomain contract
-        subdomainRegistrar = await SubdomainRegistrar.new(ens.address,resolver.address,defaultRevResolver.address,namehash('transcoder.eth'),'transcoder.eth');
+        subdomainRegistrar = await SubdomainRegistrar.new(ens.address, resolver.address, namehash('transcoder.eth'), 'transcoder.eth');
 
         //Transfer ownership of transcoder.eth to our new contract instance
-        await ens.setOwner(namehash('transcoder.eth'), subdomainRegistrar.address, {from :accounts[1]});
+        await ens.setOwner(namehash('transcoder.eth'), subdomainRegistrar.address, { from: accounts[1] });
         assert.equal(await ens.owner(namehash('transcoder.eth')), subdomainRegistrar.address);
+    });    
 
-        //Following transctions will be called from the livepeer server
-        //Transaction 1: Assign reverse address ownership to the register subdomain contract
-        await reverseRegistrar.claim(subdomainRegistrar.address, {from: accounts[2]});
-
-        //Transaction 2 : Create a subdomain/reverse mapping for the account
+    it('register and transfer subdomain', async () => {
+        //Transaction 1 : Create a subdomain for the account
         await subdomainRegistrar.registerSubdomain('testing', {from : accounts[2]});
 
         //Test we setup resolvers correctly
         assert.equal(await ens.resolver(namehash('testing.transcoder.eth')),resolver.address);
-        assert.equal(await ens.resolver(namehash(accounts[2].slice(2).toLowerCase() + ".addr.reverse")), defaultRevResolver.address);
-
-        //Test reverse resolution returns right name
-        reverseResolvedName = await defaultRevResolver.name(namehash(accounts[2].slice(2).toLowerCase() + ".addr.reverse"));
-        assert.equal(reverseResolvedName,'testing.transcoder.eth');
 
         //Test resolution of domain returns correct address
         resolvedAddr =await resolver.addr(namehash('testing.transcoder.eth'));
@@ -63,5 +47,38 @@ contract('Subdomain Registrar', function (accounts) {
         assert.equal(await ens.owner(namehash('testing.transcoder.eth')), accounts[2]);
         await subdomainRegistrar.transferNodeOwnership(web3.sha3('testing'),accounts[3]);
         assert.equal(await ens.owner(namehash('testing.transcoder.eth')), accounts[3]);
+    });
+
+    it('transfer ownership of subdomain to different address', async () => {
+        assert.equal(await ens.owner(namehash('transcoder.eth')), subdomainRegistrar.address);
+        await subdomainRegistrar.transferDomainOwnership(accounts[3],{from : accounts[0]});
+        assert.equal(await ens.owner(namehash('transcoder.eth')), accounts[3]);
+    });
+
+    it('transfer ownership of subdomain to different address', async () => {
+        await subdomainRegistrar.registerSubdomain('testing2', { from: accounts[2] });
+        await subdomainRegistrar.registerSubdomain('testing3', { from: accounts[3] });
+
+        //Create a new subdomain Registrar & transfer ownership to it
+        newSubdomainRegistrar = await SubdomainRegistrar.new(ens.address, resolver.address, namehash('transcoder.eth'), 'transcoder.eth');
+        await subdomainRegistrar.transferDomainOwnership(newSubdomainRegistrar.address, { from: accounts[0] });
+        assert.equal(await ens.owner(namehash('transcoder.eth')), newSubdomainRegistrar.address);       
+
+        //This subodomain registrar should be able to re-assign ownerships of the above two subdomains
+        await newSubdomainRegistrar.transferNodeOwnership(web3.sha3('testing1'), accounts[4]);
+        await newSubdomainRegistrar.transferNodeOwnership(web3.sha3('testing2'), accounts[5]);
+
+        assert.equal(await ens.owner(namehash('testing1.transcoder.eth')), accounts[4]);
+        assert.equal(await ens.owner(namehash('testing2.transcoder.eth')), accounts[5]);
+    });
+
+    it('should fail to create duplicate subdomain', async () => {       
+        await subdomainRegistrar.registerSubdomain('testing1', { from: accounts[1] });
+        try {
+            await subdomainRegistrar.registerSubdomain('testing1', { from: accounts[2] });
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        assert.fail('Duplicate subdomain was registered!');
     });
 });
